6f26e8df32dba7abb8c876947a67d87e1e6828ab
atifbashir-ju <atifparay16@gmail.com>
2025-10-27 21:18:57 +0530

    sixth commit

---
+++ codechief/functionsandponiters/basicsfuntion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void usa() {
+ cout<<"you are in usa "<<endl;
+ return;
+}
+void india() {
+    cout<<"you are in india"<<endl;
+    return;
+}
+int main() {
+     usa();
+    cout<<"you are in main "<<endl;
+   
+    india();
+
+
+}

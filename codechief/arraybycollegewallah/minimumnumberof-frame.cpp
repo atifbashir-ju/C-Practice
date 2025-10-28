2ccba4309f1e308a2ea3ec11b2dc84458388c95f
atifbashir-ju <atifparay16@gmail.com>
2025-10-28 22:19:36 +0530

    commit 8th

---
+++ codechief/arraybycollegewallah/minimumnumberof-frame.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<int> A(n);
+        for(int i = 0; i<n; i++) 
+        cin>> A[i];
+    }
+}

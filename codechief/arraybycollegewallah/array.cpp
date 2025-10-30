9cdb28c83bf264edc9d9752dc191856be7b4589d
atifbashir-ju <atifparay16@gmail.com>
2025-10-31 00:21:06 +0530

    9th commit

---
+++ codechief/arraybycollegewallah/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    cout<< "Element of the array are: "<<endl;
+
+    for(int i = 0; i< 5; i++){
+        cout<< "arr["<< i << "] = " <<arr[i]<<endl;
+    }
+    return 0;
+}

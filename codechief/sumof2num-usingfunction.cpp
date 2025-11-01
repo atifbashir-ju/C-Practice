a9818ddc2e3dbc3c67fd1fb56051505058418db5
atifbashir-ju <atifparay16@gmail.com>
2025-11-01 20:41:18 +0530

    11th commit

---
+++ codechief/sumof2num-usingfunction.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+    int add(int a, int b) {
+        return a + b;
+
+    }
+    int main() {
+        int sum = add(5, 8);
+        cout<<"sum = "<<sum;
+
+        return 0;
+    }
+    

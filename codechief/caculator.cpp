29ab662d662620a855c437ef66aeb746bb30a2fb
atifbashir-ju <atifparay16@gmail.com>
2025-11-01 22:34:54 +0530

    12th commit

---
+++ codechief/caculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op; double a, b;
+    cout << "Enter operator (+,-,*,/): ";
+    cin >> op;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    switch (op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': 
+            if (b != 0) cout << a / b;
+            else cout << "Error: divide by zero";
+            break;
+        default: cout << "Invalid operator";
+    }
+}

bf8c7313de0b33bf6a9778304c289104fe9793a2
atifbashir-ju <atifparay16@gmail.com>
2025-11-05 21:33:45 +0530

    14th commit

---
+++ codechief/array/max.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getmax(int num[], int n){
+    int max = INT_MIN;
+    for(int i = 0; i<n; i++) {
+        if(num[i]>max){
+            max= num[i];
+
+        }
+    }
+    return max;
+}
+int main() {
+    int size;
+    cin>>size;
+    int num[100];
+    for(int i =0; i<size; i++){
+        cin>>num[i];
+
+    }
+    cout<<"maximum value is "<<getmax(num, size)<<endl;
+    return 0;
+
+}
